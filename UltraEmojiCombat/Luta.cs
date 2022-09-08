9d081144626f36b67fb42833fe881bacb21dcd8f
ElmineiroStone <paulo.jesus@stone.com.br>
2022-09-08 12:27:39 -0300

    Adicionar arquivos de projeto.

---
+++ UltraEmojiCombat/Luta.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltraEmojiCombat
+{
+    public class Luta
+    {
+        private bool aprovada;
+
+        public Lutador Desafiado { get; set; }
+        public Lutador Desafiante { get; set; }
+        public int Rounds { get; set; }
+        public bool Aprovada { get; set; }
+
+        //metodos publicos
+        public void marcarLuta(Lutador l1, Lutador l2)
+        {
+            if (l1.Categoria == l2.Categoria && l1 != l2)
+            {
+                aprovada = true;
+                Desafiado = l1;
+                Desafiante = l2;
+            }
+            else
+            {
+                aprovada = false;
+                Desafiado = null;
+                Desafiante = null;
+            }
+        }
+        public void luta()
+        {
+            if (aprovada)
+            {
+                Console.WriteLine("============ A LUTA VAI COMEÇAR ===========");
+                Console.WriteLine("\n### DESAFIADO ###");
+                Desafiado.Apresentar();
+                Console.WriteLine("\n### DESAFIANTE ###");
+                Desafiante.Apresentar();
+
+                Random aleatorio = new Random();
+                int vencedor = aleatorio.Next(3); // 0 1 2
+
+                Console.WriteLine("\n=========== RESULTADO DA LUTA ===========");
+
+                switch (vencedor)
+                {
+                    case 0: // empate
+                        Console.WriteLine("Empatou!");
+                        Desafiado.EmpatarLuta();
+                        Desafiante.EmpatarLuta();
+                        break;
+                    case 1: // desafiado vence
+                        Console.WriteLine($"Vitória do {Desafiado.Nome}");
+                        Desafiado.GanharLuta();
+                        Desafiante.PerderLuta();
+                        break;
+                    case 2: // desafiante vence
+                        Console.WriteLine($"Vitória do {Desafiante.Nome}");
+                        Desafiante.GanharLuta();
+                        Desafiado.PerderLuta();
+                        break;
+                }
+                Console.WriteLine("================================\n");
+            }
+            else
+            {
+                Console.WriteLine("A luta não pode acontecer!");
+            }
+        }
+
+
+    }
+}
---
+++ UltraEmojiCombat/Lutador.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltraEmojiCombat
+{
+    public class Lutador
+    {
+        private float peso;
+        private string categoria;
+
+        //metodos publicos
+        public void Apresentar()
+        {
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine($"CHEGOU A HORA! Apresentamos o lutador {Nome}");
+            Console.WriteLine($"Diretamente de {Nacionalidade}");
+            Console.WriteLine($"com {Idade} anos e {Altura}");
+            Console.WriteLine($"pesando {Peso} Kg");
+            Console.WriteLine($"{Vitorias} vitórias");
+            Console.WriteLine($"{Derrotas} derrotas e");
+            Console.WriteLine($"{Empates} empates!");
+        }
+
+        public void Status()
+        {
+            Console.WriteLine($"{Nome} é um peso {Categoria}");
+            Console.WriteLine($"Ganhou {Vitorias} vezes");
+            Console.WriteLine($"Perdeu {Derrotas} vezes");
+            Console.WriteLine($"Empatou {Empates} vezes");
+        }
+
+        public void GanharLuta()
+        {
+            Vitorias++;
+        }
+        public void PerderLuta()
+        {
+            Derrotas++;
+        }
+        public void EmpatarLuta()
+        {
+            Empates++;
+        }
+        
+        
+        //metodos especiais
+        public Lutador(string no, string na, int id, float alt, float pe, int vit,
+            int de, int emp)
+        {
+            Nome = no;
+            Nacionalidade = na;
+            Idade = id;
+            Altura = alt;
+            Peso = pe;
+            Vitorias = vit;
+            Derrotas = de;
+            Empates = emp;
+        }
+
+        public string Nome { get; set; }
+        public string Nacionalidade { get; set; }
+        public int Idade { get; set; }
+        public float Peso
+        {
+            get => peso;
+            set
+            {
+                peso = value;
+                Categoria = categoria;
+            }
+        }
+        
+        public float Altura { get; set; }
+        public int Vitorias { get; set; }
+        public int Derrotas { get; set; }
+        public int Empates { get; set; }
+        public string Categoria { 
+            get 
+            { 
+                return categoria;
+            }
+            set
+            {
+                if (peso < 52.2)
+                {
+                    categoria = "Inválido";
+                }
+                else if (peso <= 70.3)
+                {
+                    categoria = "Leve";
+                }
+                else if (peso <= 83.9)
+                {
+                    categoria = "Médio";
+                }
+                else if (peso <= 120.2)
+                {
+                    categoria = "Pesado";
+                }
+                else
+                {
+                    categoria = "Inválido";
+                }
+            }
+        }
+        
+    }
+}
